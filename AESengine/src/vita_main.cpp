a6b7b88845c6bb7949a56c861c68d82ba9aea7ac
blackbird806 <blackbirdcry806@gmail.com>
2022-02-25 14:16:34 +0100

    Wip vita 3d rendering

--- AESengine/src/vita_main.cpp
+++ AESengine/src/vita_main.cpp
@@ -25,21 +25,12 @@ public:
 	aes::RHITexture texture;
 	aes::RHIFragmentShader fragmentShader;
 	aes::RHIVertexShader vertexShader;
-	aes::Draw2d draw2d;
 	aes::Camera camera;
 
 	Game(InitInfo const& info) : Engine(info)
 	{
 		AES_LOG("Game initialized");
 	}
-
-	//struct Vertex
-	//{
-	//	glm::vec2 pos;
-	//	//uint32_t color;
-	//	//glm::vec2 uv;
-	//	glm::vec4 color;
-	//};
 
 	void start() override
 	{
@@ -96,7 +87,7 @@ public:
 		vertexInputLayout[0].parameterName = "aPosition";
 		vertexInputLayout[0].semantic = aes::SemanticType::Position;
 		vertexInputLayout[0].offset = 0;
-		vertexInputLayout[0].format = RHIFormat::R32G32_Float;
+		vertexInputLayout[0].format = RHIFormat::R32G32B32_Float;
 
 		//vertexInputLayout[1].parameterName = "aTexCoord";
 		//vertexInputLayout[1].semantic = aes::SemanticType::TexCoord;
@@ -105,7 +96,7 @@ public:
 
 		vertexInputLayout[1].parameterName = "aColor";
 		vertexInputLayout[1].semantic = aes::SemanticType::Color;
-		vertexInputLayout[1].offset = sizeof(glm::vec2) * 1;
+		vertexInputLayout[1].offset = sizeof(glm::vec3) * 1;
 		//vertexInputLayout[1].format = RHIFormat::R8G8B8A8_Uint;
 		vertexInputLayout[1].format = RHIFormat::R32G32B32A32_Float;
 
@@ -138,13 +129,12 @@ public:
 		texture.init(textureDesc);
 		AES_LOG("texture init");
 
-		draw2d.init();
-
 		// enable sticks
 		sceCtrlSetSamplingMode(SCE_CTRL_MODE_ANALOG_WIDE);
 	}
 
-	float speed = 5.0f;
+	float speed = 1.0f;
+	glm::mat4 transform = glm::translate(glm::mat4(1.0f), glm::vec3(0.5f, 0.0f, 0.2f));
 
 	void update(float dt) override
 	{
@@ -153,28 +143,47 @@ public:
 		SceCtrlData ct;
 		int res = sceCtrlReadBufferPositive(0, &ct, 1);
 		AES_ASSERT(res >= 0);
-		
+
+		if (ct.buttons & SCE_CTRL_DOWN)
+		{
+			//camera.pos.z -= speed * dt;
+			transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, speed * dt));
+		}
+		if (ct.buttons & SCE_CTRL_UP)
+		{
+			//camera.pos.z += speed * dt;
+			transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, -speed * dt));
+		}
+
+		if (ct.buttons & SCE_CTRL_RIGHT)
+		{
+			transform = glm::translate(transform, glm::vec3(speed * dt, 0.0f, 0.0f));
+		}
+
+		if (ct.buttons & SCE_CTRL_LEFT)
+		{
+			transform = glm::translate(transform, glm::vec3(-speed * dt, 0.0f, 0.0f));
+		}
+
 		if (ct.ly > 150)
 		{
 			camera.pos.x += speed;
+		}
+		if (ct.ly < 100)
+		{
+			camera.pos.x -= speed;
 		}
 
 		uint windowWidth = 960, windowHeight = 544;
 		float const aspect = (float)windowWidth / (float)windowHeight;
 
 		camera.lookAt(camera.pos + glm::vec3(0, 0, 1));
-		camera.projMatrix = glm::perspectiveLH_ZO(glm::radians(45.0f), aspect, 0.1f, 100.0f);
-		aes::CameraBuffer const camBuf{ glm::transpose(mainCamera.viewMatrix), glm::transpose(mainCamera.projMatrix) };
+		camera.projMatrix = glm::perspectiveLH_ZO(glm::radians(45.0f), aspect, 0.01f, 100.0f);
+		//aes::CameraBuffer const camBuf{ glm::transpose(mainCamera.viewMatrix), glm::transpose(mainCamera.projMatrix) };
+		aes::CameraBuffer const camBuf{ mainCamera.viewMatrix, mainCamera.projMatrix };
 		viewBuffer.setDataFromPOD(camBuf);
-		glm::mat4 identity(1.0f);
-		modelBuffer.setDataFromPOD(identity);
-		//draw2d.setMatrix(glm::mat4(1.0f));
-		//draw2d.setColor(aes::Color::Blue);
-		//draw2d.drawLine({ {0.0f, 0.0f}, {1.0f, 0.0f} });
-		//draw2d.setColor(aes::Color::Green);
-		//draw2d.drawLine({ {0.0f, 0.0f}, {0.0f, 1.0f} });
-		//draw2d.setColor(aes::Color::Red);
-		//draw2d.drawFillRect({ {0.5f, 0.5f}, {0.6f, 0.6f} });
+		//transform = glm::rotate(transform, speed * dt, glm::vec3(1.0f, 1.0f, 1.0f));
+		modelBuffer.setDataFromPOD(transform);
 	}
 
 	void draw() override
@@ -195,7 +204,6 @@ public:
 		context.bindIndexBuffer(indexBuffer, IndexTypeFormat::Uint16);
 		context.bindFragmentTexture(texture, 0);
 		context.drawIndexed(36, 0);
-		//draw2d.executeDrawCommands();
 	}
 };
 
